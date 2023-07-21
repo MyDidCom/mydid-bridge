6010ab590946cd5b318bfae6881271d70d916ff8
maxime_dutres <maxime.dutres@mydid.com>
2023-07-21 09:56:48 +0200

    Remove logo from header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="px-3">
-    <router-link to="/"
-      ><img
-        :src="require('/assets/images/logo_mydid.png')"
-        alt=""
-        class="w-16 mx-auto py-5"
-    /></router-link>
-  </div>
+  <div></div>
 </template>
 <script>
 export default {
